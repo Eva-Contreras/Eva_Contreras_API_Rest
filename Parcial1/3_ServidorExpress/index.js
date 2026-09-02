d05b00c3cb9883d607cb820bbcc4b35f123b599a
Eva Contreras <evacontreras425@gmail.com>
2026-09-02 16:20:21 -0600

    Middleware

--- Parcial1/3_ServidorExpress/index.js
+++ Parcial1/3_ServidorExpress/index.js
@@ -1,16 +1,40 @@
 import express from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
 import peliculasRouter from './router/peliculasRouter.js';
 
 const app = express();
-
 const port = 3000;
 
+const accesos = fs.createWriteStream('./accesos.log', { flags: 'a' });
+
+const horarioLaboral = (req, res, next) => {
+    const fecha = new Date();
+    const dia = fecha.getDay();
+    const hora = fecha.getHours();
+
+    const esDiaLaboral = dia >= 1 && dia <= 5;
+    const estaEnHorario = hora >= 8 && hora < 15;
+
+    if (!esDiaLaboral || !estaEnHorario) {
+        return res.status(503).json({
+            mensaje: 'No se pueden ejecutar peticiones a esta hora'
+        });
+    }
+
+    next();
+};
+
+// Estos son middlewares de aplicación que se ejecuta antes de las rutas
 app.use(express.json());
+app.use(morgan('combined', { stream: accesos }));
+app.use(horarioLaboral);
 
 app.get('/', (req, res) => {
     res.send('¡API de Películas funcionando!');
 });
 
+// Aquí se invocan las rutas
 app.use('/peliculas', peliculasRouter);
 
 app.listen(port, () => {
--- Parcial1/3_ServidorExpress/router/peliculasRouter.js
+++ Parcial1/3_ServidorExpress/router/peliculasRouter.js
@@ -3,10 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 const peliculas = [
-    { id: 1, titulo: "Inception", director: "Christopher Nolan", año: 2010 },
-    { id: 2, titulo: "Interstellar", director: "Christopher Nolan", año: 2014 },
-    { id: 3, titulo: "TinkerBell", director: "Bradley Raymond", año: 2008 },
-    { id: 4, titulo: "The Matrix", director: "Wachowski Sisters", año: 1999 }
+    { id: 1, titulo: "Inception", director: "Christopher Nolan", anio: 2010 },
+    { id: 2, titulo: "Interstellar", director: "Christopher Nolan", anio: 2014 },
+    { id: 3, titulo: "TinkerBell", director: "Bradley Raymond", anio: 2008 },
+    { id: 4, titulo: "The Matrix", director: "Wachowski Sisters", anio: 1999 }
 ];
 
 router.get('/', (req, res) => {
@@ -17,19 +17,16 @@ router.get('/', (req, res) => {
             p => p.titulo.toLowerCase().includes(req.query.titulo.toLowerCase())
         );
     }
-
     if (req.query.director) {
         resultado = resultado.filter(
             p => p.director.toLowerCase().includes(req.query.director.toLowerCase())
         );
     }
-
-    if (req.query.año) {
+    if (req.query.anio) {
         resultado = resultado.filter(
-            p => p.año === parseInt(req.query.año)
+            p => p.anio === parseInt(req.query.anio)
         );
     }
-
     if (resultado.length === 0) {
         return res.status(404).json({
             mensaje: "Película no encontrada"
