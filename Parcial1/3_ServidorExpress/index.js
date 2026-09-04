dc2b2a236baf89c1adbad92919f0e21349c676e6
Eva Contreras <evacontreras425@gmail.com>
2026-09-04 07:49:05 -0500

    Implementación de multer

--- Parcial1/3_ServidorExpress/index.js
+++ Parcial1/3_ServidorExpress/index.js
@@ -1,12 +1,43 @@
 import express from 'express';
 import morgan from 'morgan';
+import multer from 'multer';
 import fs from 'fs';
+import path from 'path';
 import peliculasRouter from './router/peliculasRouter.js';
 
 const app = express();
 const port = 3000;
-
 const accesos = fs.createWriteStream('./accesos.log', { flags: 'a' });
+const metadataPath = './metadata.json';
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, callback) => {
+        callback(null, path.basename(file.originalname));
+    }
+});
+const upload = multer({ storage });
+const obtenerFechaLocal = () => new Date().toLocaleString('es-MX', {
+    dateStyle: 'short',
+    timeStyle: 'medium'
+});
+
+const guardarMetadata = archivo => {
+    let archivos = [];
+
+    if (fs.existsSync(metadataPath)) {
+        const contenido = fs.readFileSync(metadataPath, 'utf8');
+        archivos = contenido ? JSON.parse(contenido) : [];
+    }
+
+    archivos.push({
+        nombre: archivo.filename,
+        tipo: archivo.mimetype,
+        tamaño: archivo.size,
+        fechaSubida: obtenerFechaLocal(),
+    });
+
+    fs.writeFileSync(metadataPath, JSON.stringify(archivos, null, 2));
+};
 
 morgan.token('local-date', () => {
     const fecha = new Date();
@@ -25,7 +56,7 @@ const horarioLaboral = (req, res, next) => {
     const hora = fecha.getHours();
 
     const esDiaLaboral = dia >= 1 && dia <= 5;
-    const estaEnHorario = hora >= 8 && hora < 15;
+    const estaEnHorario = hora >= 7 && hora < 14;
 
     if (!esDiaLaboral || !estaEnHorario) {
         return res.status(503).json({
@@ -38,11 +69,26 @@ const horarioLaboral = (req, res, next) => {
 
 // Estos son middlewares de aplicación que se ejecuta antes de las rutas
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 app.use(morgan(':remote-addr - :remote-user [:local-date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', { stream: accesos }));
 app.use(horarioLaboral);
 
 app.get('/', (req, res) => {
     res.send('¡API de Películas funcionando!');
+});
+
+app.post("/subir", upload.single("archivo"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({
+            mensaje: 'Debes enviar un archivo con el campo "archivo"'
+        });
+    }
+
+    guardarMetadata(req.file);
+
+    res.json({
+        mensaje: `Archivo recibido`
+    });
 });
 
 // Aquí se invocan las rutas
